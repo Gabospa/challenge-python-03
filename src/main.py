4dc73463ca2e3ddef617193454c309e142166a69
Gabriel <gabospa@gmail.com>
2020-06-20 16:12:00 -0500

    Mensaje Encontrado

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 # Resolve the problem!!
+import re   
+
+
 
 
 def run():
     # Start coding here
+    text = ''
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        text= f.read()
+        
+    message = re.findall(r'[a-z]+', text)    
+    print(message)
 
 
 if __name__ == '__main__':
